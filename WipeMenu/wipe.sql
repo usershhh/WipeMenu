5b5f1be3cb0128763b148ba610a27d0dbac77e39
Klayzz <81190421+Klayzz@users.noreply.github.com>
2022-01-15 20:03:40 +0100

    Add files via upload

---
+++ WipeMenu/wipe.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `wipe` (
+  `steamid` varchar(100) NOT NULL,
+  `raison` varchar(500) NOT NULL,
+  `user` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
